a491282d469e4b53c86469e6dd0f214b024da633
ahriley <30327239+ahriley@users.noreply.github.com>
2018-04-24 14:08:53 -0500

    utils.py now useful functions

--- utils.py
+++ utils.py
@@ -1,98 +1,45 @@
 import pandas as pd
 import numpy as np
+from sklearn.metrics import mean_squared_error
 
-DATA_DIR = 'data/'
+def cosine_sim(df):
+    vals = df.values
+    sim = vals @ vals.T
+    norms = np.sqrt(np.diagonal(sim))
+    return sim / norms / norms.T
 
-# ratings.csv -- 'userId' gave 'movieId' a 'rating' at 'timestamp'
-# 20,000,263 individual ratings
-def load_ratings(filename=None,tstamp=False):
-    if filename is None:
-        filename = DATA_DIR+'ratings.csv'
+def mse(pred, actual):
+    pred = pred.values[actual.values > 0]
+    actual = actual.values[actual.values > 0]
+    return mean_squared_error(pred, actual)
 
-    df = pd.read_csv(filename)
-    if not tstamp:
-        df.drop(columns=['timestamp'], inplace=True)
+def predict_full(ratings, weights):
+    prediction = weights.dot(ratings) / np.array([np.abs(weights).sum(axis=1)]).T
+    return pd.DataFrame(data=prediction, index=ratings.index, columns=ratings.columns)
 
-    return df
-
-# movies.csv -- 'movieId' has 'title' and 'genres', 27,278 movies
-# NOTE: flat=False reads processed pickle file with genre data as cols
-def load_movies(filename=None,flat=False):
-    if filename is None:
-        filename = DATA_DIR+'movies.csv'
-
-    df = pd.read_csv(filename,index_col='movieId')
-    """
-    # code used to generate processed/movies_unflattened.pkl
-    if not flat:
-        # figure out list of possible genres
-        genreset = set()
-        for index, row in df.iterrows():
-            genrelist = row['genres'].split('|')
-            genreset = genreset.union(set(genrelist))
-
-        # create columns (0 means genre doesn't apply)
-        for genre in genreset:
-            movies[genre] = 0
-
-        for index, row in df.iterrows():
-            genrelist = row['genres'].split('|')
-            for genre in genrelist:
-                df.loc[index, genre] = 1
-
-        df.drop(columns=['genres'], inplace=True)
-    """
-    if not flat:
-        df = pd.read_pickle('processed/movies_unflattened.pkl')
-        df.set_index('movieId',inplace=True)
-
-    return df
-
-# maps 10,381 movieIds to 1,128 genome tags via a relevance score
-def load_genome(filename=None, flat=False):
-    if filename is None:
-        filename = DATA_DIR+'genome-scores.csv'
-
-    df = pd.read_csv(filename)
-
-    if not flat:
-        movies = sorted(list(set(df['movieId'])))
-        labels = np.arange(1,1128+1,1)
-
-        rel = df.as_matrix(columns=['relevance'])
-        rel = np.reshape(rel, (10381, 1128))
-        matr = np.insert(rel, 0, movies, axis=1)
-
-        df = pd.DataFrame(matr)
-        df.set_index(0, inplace=True)
-        df.index.names = ['movieId']
-        df.index = df.index.map(int)
-
-    return df
-
-# maps tagId to a tag: e.g. "zombies", "slavery"; 1,128 different tags
-def load_genometags(filename=None):
-    if filename is None:
-        filename = DATA_DIR+'genome-tags.csv'
-
-    return pd.read_csv(filename, index_col='tagId')
-
-# 'userId' gave 'movieId' a 'tag' at 'timestamp', 465,564 rows
-# NOTE: probably not useful, since it's user-specific input
-def load_tags(filename=None,tstamp=False):
-    if filename is None:
-        filename = DATA_DIR+'tags.csv'
-
-    df = pd.read_csv(filename)
-    if not tstamp:
-        df.drop(columns=['timestamp'], inplace=True)
-
-    return df
-
-# links 'movieId' to 'imdbId' and 'tmdbId'
-# NOTE: probably not useful, only connects different databases
-def load_links(filename=None):
-    if filename is None:
-        filename = DATA_DIR+'links.csv'
-
-    return pd.read_csv(filename)
+# TODO: don't go through numpy, just use pandas
+def train_test_split(df, empty, testfrac=0.2):
+    if np.isnan(empty):
+        df.replace(to_replace=np.nan, value=0.0, inplace=True)
+    elif empty != 0.0:
+        raise NotImplementedError("'empty' must be 0.0 or np.nan")
+        
+    vals = df.values
+    test = np.zeros(vals.shape)
+    train = vals.copy()
+    for user in range(vals.shape[0]):
+        nonzero = vals[user, :].nonzero()[0]
+        test_ratings = np.random.choice(nonzero, size=int(testfrac*len(nonzero)), replace=False)
+        train[user, test_ratings] = 0.0
+        test[user, test_ratings] = vals[user, test_ratings]
+        
+    # Test and training are actually disjoint
+    assert(np.all((train * test) == 0))
+    
+    train = pd.DataFrame(data=train, index=df.index, columns=df.columns)
+    test = pd.DataFrame(data=test, index=df.index, columns=df.columns)
+    if np.isnan(empty):
+        test.replace(to_replace=0.0, value=np.nan, inplace=True)
+        train.replace(to_replace=0.0, value=np.nan, inplace=True)
+    
+    return train, test
